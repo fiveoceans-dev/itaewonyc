0bd0de4d174c989973ebd7875435517983cc798a
five <niip42@gmail.com>
2025-04-12 21:47:09 +0900

    lang

--- app.js
+++ app.js
@@ -1,50 +1,56 @@
 // app.js
 
 const express = require('express');
+const session = require('express-session');
 const path = require('path');
 const i18n = require('i18n');
-const session = require('express-session');
 
 const app = express();
 
-// -------------------
 // i18n Configuration
-// -------------------
 i18n.configure({
   locales: ['ko', 'en'],
   directory: path.join(__dirname, 'locales'),
   defaultLocale: 'ko',
   cookie: 'lang',
-  queryParameter: 'lang',
   autoReload: true,
   updateFiles: false
 });
 
 app.use(i18n.init);
 
-// Make translation function available in all views
+// Locale Extraction Middleware
 app.use((req, res, next) => {
-  res.locals.__ = res.__;
-  res.locals.locale = res.getLocale(); // Optional: for language switch buttons
+  const segments = req.url.split('/');
+  // segments[0] is empty because URL starts with "/"
+  if (segments.length > 1) {
+    const potentialLocale = segments[1];
+    if (['ko', 'en'].includes(potentialLocale)) {
+      req.setLocale(potentialLocale);
+      res.locals.locale = potentialLocale;
+      req.url = req.url.substring(potentialLocale.length + 1) || '/';
+    }
+  }
   next();
 });
 
-// -------------------
+// Make translation function available in all views
+app.use((req, res, next) => {
+  res.locals.__ = res.__;
+  res.locals.locale = res.getLocale();
+  next();
+});
+
 // View Engine & Static
-// -------------------
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// -------------------
 // Routes
-// -------------------
 const viewsRoutes = require('./routes/views');
 app.use('/', viewsRoutes);
 
-// -------------------
 // Error Handling
-// -------------------
 app.use((req, res) => {
   res.status(404).render('404', { title: res.__('Page Not Found') });
 });
